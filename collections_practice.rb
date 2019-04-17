0ed8fecf0927b5222af513da5376322769df9b7e
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-17 00:55:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+  end 
+end 
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a} 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end 
+end 
+
+def swap_elements(array)
+  second = array[1] 
+  third = array[2]
+  array[1] = third
+  array[2] = second
+  array 
+end 
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end 
+end 
